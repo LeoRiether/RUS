a7b891abe109b3d87d2e65effda59160bbf8f0bf
LeoRiether <leonardoalves8217@gmail.com>
2021-02-13 11:42:16 -0300

    Parse commands

---
+++ Parser.fs
@@ -0,0 +1,24 @@
+module Parser
+
+type Url = Url of string
+type PushData = Url
+type PopData = Url
+
+type Command =
+    | Help
+    | Peek
+    | Push of PushData
+    | Pop of PopData
+
+let parseArgs = function
+    | [] | "help"::_ | "--help"::_ -> Ok Help
+
+    | "peek"::_ -> Ok Peek
+
+    | ["push"] -> Error "push needs at least one argument"
+    | "push"::url::_ -> Ok (Push (Url url))
+
+    | ["pop"] -> Error "pop needs at least one argument"
+    | "pop"::url::_ -> Ok (Pop (Url url))
+
+    | unknown::_ -> Error (sprintf "Unknown command \"%s\"" unknown)
--- Program.fs
+++ Program.fs
@@ -1,6 +1,48 @@
+open System
+open Parser
+
+let dataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+let dataFile = dataFolder + "/rus.data"
+
+[<Literal>]
+let HelpString = """R.U.S. – Randomized Unsolved Stack
+
+Usage:
+    rus [command] [options]
+
+Commands:
+    help             Displays this help message
+    push <url>       Pushes an URL onto the stack
+    pop <url>        Pops an URL from the stack
+    peek             Peeks a random item from near the top of the stack
+
+Examples:
+    rus push https://codeforces.com/problemset/problem/4/A
+    rus peek
+    rus pop http://codeforces.com/problemset/problem/4/A
+"""
+
+let doPeek () =
+    ()
+
+let doPush data =
+    ()
+
+let doPop data =
+    ()
+
+let execute = function
+    | Help -> printfn "%s" HelpString
+    | Peek -> doPeek ()
+    | Push x -> doPush x
+    | Pop x  -> doPop x
+
 [<EntryPoint>]
 let main argv =
+    let command = parseArgs (List.ofArray argv)
 
-    printfn "Hello World!"
+    match command with
+    | Ok cmd -> execute cmd
+    | Error e -> failwith e
 
     0
